f281984d8c09cacf313d168473fc799747555480
Akashbora02 <akashbora0082.com>
2026-02-19 18:30:21 +0530

    All Updated

---
+++ FlightReservationApplication/backend.groovy
@@ -0,0 +1,37 @@
+pipeline{
+    agent any
+    stages{
+        stage('PULL'){
+            steps{
+                git branch: 'main', url: 'https://github.com/Akashbora02/Flight-Reservation.git'
+            }
+        }
+        stage('BUILD'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    docker build -t akashbora02/flight-reservation-backend:latest .
+                    docker push akashbora02/flight-reservation-backend:latest
+                '''
+            }
+        }
+        stage('TEST'){
+            steps{
+                    withSonarQubeEnv(credentialsId: 'sonar-token', installationName: 'sonar') {
+                    sh'''
+                    cd FlightReservationApplication
+                    mvn sonar:sonar -Dsonar.projectKey=Flight-Reservation
+                    '''
+                    }
+            }
+        }
+        stage('DEPLOY'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    kubectl apply -f k8s/
+                '''
+            }
+        }
+    }
+}
